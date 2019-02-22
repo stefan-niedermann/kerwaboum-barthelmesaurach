4591220da5550f6847f60018362822accfccf22f
stefan-niedermann <info@niedermann.it>
2019-02-22 16:09:16 +0100

    Rework with materialize-css

--- searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <input type="search" placeholder="<?php echo __('Search …'); ?>" value="" name="s" id="s" />
-    </div>
-</form>
