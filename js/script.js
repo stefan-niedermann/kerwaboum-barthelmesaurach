3f25712fde3d25c17d3bb7d3a74410dbd831ed5a
stefan-niedermann <info@niedermann.it>
2019-02-24 09:23:32 +0100

    Content

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    if(!document.documentMode) { // https://github.com/Dogfalo/materialize/issues/5801
+        M.Materialbox.init(document.querySelectorAll('.materialboxed'));
+    }
+});
