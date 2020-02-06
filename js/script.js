6c2f1c6adde7fa0b0ddfc7881f5677070e2d15e6
stefan-niedermann <info@niedermann.it>
2020-02-06 16:55:55 +0100

    Add "Einladung zur JHV"

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    M.Collapsible.init(document.querySelectorAll('.collapsible'));
     if(!document.documentMode) { // https://github.com/Dogfalo/materialize/issues/5801
         M.Materialbox.init(document.querySelectorAll('.materialboxed'));
     }
