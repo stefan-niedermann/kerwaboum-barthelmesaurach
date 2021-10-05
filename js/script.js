8123cded337fd5c92cf6b044b63bebe6803413d3
Stefan Niedermann <info@niedermann.it>
2021-10-05 19:02:25 +0200

    Use more modern javascript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    M.Sidenav.init(document.querySelectorAll('.sidenav'));
-    M.Collapsible.init(document.querySelectorAll('.collapsible'));
-    if(!document.documentMode) { // https://github.com/Dogfalo/materialize/issues/5801
-        M.Materialbox.init(document.querySelectorAll('.materialboxed'));
-    }
-});
+try {
+    document.addEventListener('DOMContentLoaded', () => {
+        M.Sidenav.init(document.getElementsByClassName('sidenav'));
+        M.Collapsible.init(document.getElementsByClassName('collapsible'));
+        if (!document.documentMode) { // https://github.com/Dogfalo/materialize/issues/5801
+            M.Materialbox.init(document.getElementsByClassName('materialboxed'));
+        }
+    });
+} catch (e) {
+    console.error(e);
+}
