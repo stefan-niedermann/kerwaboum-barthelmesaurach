589b998a68dc00bb7b5c36fa047e83130cca35e6
Stefan Niedermann <info@niedermann.it>
2024-06-24 15:11:44 +0200

    fix: timer

--- src/js/KerwaCountdownComponent.js
+++ src/js/KerwaCountdownComponent.js
@@ -3,52 +3,75 @@ export class KerwaCountdownComponent extends HTMLTimeElement {
     #endDate;
     #interval;
     #kerwaStart;
+    #dayOfKerwa;
+
+    static get observedAttributes() {
+        return ['datetime'];
+    }
+
+    get kerwaStart() {
+        return this.#kerwaStart;
+    }
+
+    get dayOfKerwa() {
+        return this.#dayOfKerwa;
+    }
 
     constructor() {
         super();
-        console.log('========================= CONSTRUCTOR ==========================');
     }
 
     connectedCallback() {
-        console.log('========================= CONNECTED ==========================');
-
-        // this.attachShadow({ mode: 'open' });
-        // this.shadowRoot.appendChild(this.shadowRoot.content.cloneNode(true));
-
         this.setAttribute('id', 'timer');
-
-        this.#kerwaStart = new Date(Date.parse(this.getAttribute('datetime')))
-
-        this.#endDate = this.#kerwaStart.getTime();
-        this.#interval = setInterval(this.#calculate, 1_000);
-
-        this.#calculate();
-    }
-
-    disconnectedCallback() {
-        console.log('========================= DISCONNECTED ==========================');
+        this.#startCountdown();
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-        console.log('========================= ATTRIBUTE CHANGED ==========================')
-        console.log(name, oldValue, newValue);
-    }
-
-    #setText(now, newText) {
-        if (this.shadowRoot.textContent !== newText) {
-            this.shadowRoot.textContent = newText;
+        switch (name) {
+            case 'datetime': {
+                this.#startCountdown();
+                break;
+            }
         }
-        this.dataset.close = now.getMonth() === this.#kerwaStart.getMonth();
     }
 
-    #getKerwaStart() {
-        const currentYear = new Date().getFullYear();
-        const date = new Date(currentYear, 8, 1, 12);
-        let weekday = date.getDay();
-        let dayDiff = weekday === 0 ? 7 : weekday;
-        let lastSunday = date.setDate(date.getDate() - dayDiff - 7 - 2);
-        return date;
+    #startCountdown() {
+        const now = new Date();
+        this.#kerwaStart = new Date(Date.parse(this.getAttribute('datetime')))
+        this.#endDate = this.#kerwaStart.getTime();
+
+        if (this.#interval !== undefined) {
+            clearInterval(this.#interval);
+        }
+
+        this.#interval = setInterval(this.#calculate, 1_000);
+        this.#calculate();
+
+        if (this.#kerwaStart.getMonth() === now.getMonth() &&
+            now.getDate() >= this.#kerwaStart.getDate() &&
+            now.getDate() <= this.#kerwaStart.getDate() + 5) {
+
+            const weekdayKerwaday = new Map([[6, 2], [7, 3], [1, 4]]);
+            this.#dayOfKerwa = weekdayKerwaday.get(now.getDay());
+
+        } else {
+            this.#dayOfKerwa = undefined;
+        }
+
     }
+
+    disconnectedCallback() {
+        clearInterval(this.#interval);
+    }
+
+    // #getKerwaStart() {
+    //     const currentYear = new Date().getFullYear();
+    //     const date = new Date(currentYear, 8, 1, 12);
+    //     let weekday = date.getDay();
+    //     let dayDiff = weekday === 0 ? 7 : weekday;
+    //     let lastSunday = date.setDate(date.getDate() - dayDiff - 7 - 2);
+    //     return date;
+    // }
 
     #calculate() {
         const now = new Date();
@@ -76,11 +99,19 @@ export class KerwaCountdownComponent extends HTMLTimeElement {
 
             this.#setText(now, newText);
         } else {
-            this.remove();
+            // this.remove();
+            this.#setText(now, '');
             clearInterval(this.#interval);
             return;
         }
     };
+
+    #setText(now, newText) {
+        if (this.textContent !== newText) {
+            this.textContent = newText;
+        }
+        this.dataset.close = now.getMonth() === this.#kerwaStart.getMonth();
+    }
 }
 
 customElements.define('kerwa-countdown', KerwaCountdownComponent, { extends: 'time' });
--- src/js/script.js
+++ src/js/script.js
@@ -2,24 +2,15 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const now = new Date();
-    const date = new Date(now.getFullYear(), 8, 1, 12);
+    const date = new Date(new Date().getFullYear(), 8, 1, 12);
     const weekday = date.getDay();
     const dayDiff = weekday === 0 ? 7 : weekday;
     date.setDate(date.getDate() - dayDiff - 7 - 2);
 
-    document.querySelector('time').setAttribute('datetime', date.toISOString());
-
-    if (this.#kerwaStart.getMonth() === now.getMonth() &&
-        now.getDate() >= this.#kerwaStart.getDate() &&
-        now.getDate() <= this.#kerwaStart.getDate() + 5) {
-
-        const weekdayKerwaday = new Map([[6, 2], [7, 3], [1, 4]]);
-        const targetDayElementNumber = weekdayKerwaday.get(now.getDay());
-
-        if (targetDayElementNumber !== undefined) {
-            const targetDayElement = document.querySelector(`#timetable article:nth-child(${targetDayElementNumber})`);
-            document.querySelector(`#timetable div`).scrollLeft = targetDayElement.offsetLeft;
-        }
+    const kerwaCountdown = document.querySelector('time');
+    kerwaCountdown.setAttribute('datetime', date.toISOString());
+    if (kerwaCountdown.dayOfKerwa !== undefined) {
+        const targetDayElement = document.querySelector(`#timetable article:nth-child(${kerwaCountdown.dayOfKerwa})`);
+        document.querySelector(`#timetable div`).scrollLeft = targetDayElement.offsetLeft;
     }
 });
